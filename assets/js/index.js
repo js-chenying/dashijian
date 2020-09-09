e159d9f7eb027d7205aa4afce0e7079a0a5897e0
chenying <1821559273@qq.com>
2020-09-09 15:25:35 +0800

    文章管理结束

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ function getUseInfo() {
         //     Authorization: localStorage.getItem("token") || ''
         // },
         success: function (res) {
-            console.log(res);
+            // console.log(res);
             if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败')
             }
